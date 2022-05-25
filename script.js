13555ee48a35c76ec3db666d32131b4e800fa608
Rahul Lohra <rahullohra987@gmail.com>
2022-05-25 20:52:38 +0530

    This is a website which take care of your expenses

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let newMonth = document.querySelector('.new-month')
+let nextMonth = document.querySelector('.next-month')
+
+newMonth.addEventListener('click', function(e) {
+    console.log(e);
+})
+
+nextMonth.addEventListener('click', function(e) {
+    console.log(e);
+})
